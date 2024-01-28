0ebec373cd36ee8127a3a851a6c15ebafdee57e3
Viktoriia Chebera <cheberavika@gmail.com>
2024-01-28 20:02:07 +0200

    added console app

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть перше число: ");
+        string input = Console.ReadLine();
+
+        if (double.TryParse(input, out double firstNumber))
+        {
+            Console.Write("Введіть друге число: ");
+            input = Console.ReadLine();
+
+            if (double.TryParse(input, out double secondNumber))
+            {
+                double sum = firstNumber + secondNumber;
+
+                Console.WriteLine($"Сума чисел {firstNumber} + {secondNumber} = {sum}");
+            }
+            else
+            {
+                Console.WriteLine("Некоректне введення для другого числа.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Некоректне введення для першого числа.");
+        }
+    }
+}
